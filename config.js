2c5d9943419419039f7446e9018a5398c9f607b7
des-yogi <nuevo-design@mail.ru>
2019-06-09 19:29:45 +0300

    The first commit

---
+++ config.js
@@ -0,0 +1,44 @@
+/* global module */
+
+let config = {
+  'notGetBlocks': [
+    'blocks-demo.html',
+  ],
+  'ignoredBlocks': [
+    'no-js',
+  ],
+  'alwaysAddBlocks': [
+    // 'sprite-png',
+    'sprite-svg',
+    'object-fit-polyfill',
+  ],
+  'addStyleBefore': [
+    'src/scss/variables.scss',
+    'src/scss/mixins.scss',
+    // 'somePackage/dist/somePackage.css', // для 'node_modules/somePackage/dist/somePackage.css',
+  ],
+  'addStyleAfter': [
+    // 'src/scss/print.scss',
+  ],
+  'addJsBefore': [
+    './head-script.js',
+    // 'somePackage/dist/somePackage.js', // для 'node_modules/somePackage/dist/somePackage.js',
+  ],
+  'addJsAfter': [
+    './script.js',
+  ],
+  'addAssets': {
+    'src/fonts/*.{woff,woff2}': 'fonts/',
+    'src/img/*.{png,svg,jpg,jpeg,webp}': 'img/',
+    //'src/libs/**/*.{jse}': 'libs/',
+    // 'src/favicon/*.{png,ico,svg,xml,webmanifest}': 'img/favicon',
+    // 'node_modules/somePackage/images/*.{png,svg,jpg,jpeg}': 'img/',
+  },
+  'dir': {
+    'src': 'src/',
+    'build': 'build/',
+    'blocks': 'src/blocks/'
+  }
+};
+
+module.exports = config;
